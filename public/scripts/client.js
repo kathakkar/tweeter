17dfeb0281c1bd79c7919c4c646b1009f80f63bf
Khushbu <khushbu.thakkar@outlook.com>
2021-09-08 21:48:42 +0000

    Add jquery post tosubmit form data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,54 +4,78 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
+$(document).ready(function() {
+  
+  const loadTweets = function () {
+    $.getJSON('/tweets/', function(tweetsJSON){
+      renderTweets(tweetsJSON);
+        }).fail(function(){
+            console.log("An error has occurred.");
+    });
+  }
+  loadTweets();
+
+  const postData = function (obj) {
+    $.post('/tweets/', obj, (response) => {
+      console.log(response)
+      loadTweets();
+    })
+  }
+
+  const $form = $('#new-tweet form');
+  $form.on('submit', function(event) {
+    event.preventDefault();
+    const serializedData = $(this).serialize(); 
+    postData(serializedData);
+  });
+
 
 const createTweetElement = function (obj) {
   let time_ago_count = timeago.format(obj["created_at"]);
-  let markUp = `<article class="tweet">
-              <header>  
-              <span id="tweet_img"><img src="${obj['user']['avatars']}"></span>
-              <span id="tweet_username">${obj['user']['name']}</span>
-              <span id="tweet_user_id"><b>${obj['user']['handle']}</b></span></header>
-              <main><b>${obj['content']['text']}</b></main>
-              <footer>
-              <span id="tweet_timespan"><h6>${time_ago_count}</h6></Span>
-              <span id="tweet_icons"><i class="fas fa-flag"></i>&nbsp;&nbsp;<i class="fas fa-retweet"></i>&nbsp;&nbsp;<i class="fas fa-heart"></i></span>
-              </footer></article>`;
-  return markUp;
+
+  const $article = $('<article>').addClass('tweet');
+  const $header = $('<header>');
+  const $spanImg = $('<span>').attr('id','tweet_img').html(`<img src="${obj['user']['avatars']}">`);
+  const $spanUserName = $('<span>').attr('id','tweet_username').text(obj['user']['name']);
+  const $spanUserId = $('<span>').attr('id','tweet_user_id').text(obj['user']['handle']);
+
+  $header.append($spanImg, $spanUserName, $spanUserId);
+
+  const $main = $('<main>');
+  const $b = $('<b>').text(obj['content']['text']);
+
+  $main.append($b);
+
+  const $footer = $('<footer>');
+  const $spanTimeSpan = $('<span>').attr('id','tweet_timespan').text(time_ago_count);
+  const $spanIcons = $('<span>').attr('id','tweet_icons').html('<i class="fas fa-flag"></i>&nbsp;&nbsp;<i class="fas fa-retweet"></i>&nbsp;&nbsp;<i class="fas fa-heart"></i>');
   
+  $footer.append($spanTimeSpan, $spanIcons);
+
+  $article.append($header, $main, $footer);
+
+
+  // let markUp = `<article class="tweet">
+  //             <header>  
+  //             <span id="tweet_img"><img src="${obj['user']['avatars']}"></span>
+  //             <span id="tweet_username">${obj['user']['name']}</span>
+  //             <span id="tweet_user_id"><b>${obj['user']['handle']}</b></span></header>
+  //             <main><b>${obj['content']['text']}</b></main>
+  //             <footer>
+  //             <span id="tweet_timespan"><h6>${time_ago_count}</h6></Span>
+  //             <span id="tweet_icons"><i class="fas fa-flag"></i>&nbsp;&nbsp;<i class="fas fa-retweet"></i>&nbsp;&nbsp;<i class="fas fa-heart"></i></span>
+  //             </footer></article>`;
+  return $article;
 }
 
 const renderTweets = function (tweets) {
+  $('#tweet-container').empty();
   for(let tweet in tweets){
     const $tweet = createTweetElement(tweets[tweet]);
-    $('#tweet-container').append($tweet);
+    $('#tweet-container').prepend($tweet);
   }
-
 }
 
-$(document).ready(function() {
-  // $(form).on('submit',function (event){
-  //   event.preventDefault();
-  //   alert("in");
-  // });
-  $.getJSON("/tweets/", function(tweetsJSON){
-    renderTweets(tweetsJSON);
-    }).fail(function(){
-        console.log("An error has occurred.");
-    });
-    // const $tweet = createTweetElement(tweetData);
-    // $('#tweet-container').append($tweet); 
 });
 
 
