137b7c8bde09ec81b4f90cfba646e2e925dc6c3b
Khushbu <khushbu.thakkar@outlook.com>
2021-09-09 21:05:28 +0000

    Change char counter login with fewer lines of code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,23 +1,37 @@
 $(document).ready(function() {
 
   const limit = 140;
+  let resultCount = 0;
   $('#tweet-text').on('keypress', function(event) {  
-    let charcounter = Number($('#tweet-counter').val());
-    let max_limt = Number($('#max_limit').val());
-    if(charcounter >= limit){
-      charcounter = 0;
-      max_limt = limit;
-      $('#max_limit').val(max_limt);
-      $('#tweet-counter').val(charcounter);
+     let $charcounter = Number($('#tweet-counter').val());
+    // let max_limt = Number($('#max_limit').val());
+     let $tweetTextLength = Number($('#tweet-text').val().length) + 1;
+    // if(charcounter >= limit){
+    //   charcounter = 0;
+    //   max_limt = limit;
+    //   $('#max_limit').val(max_limt);
+    //   $('#tweet-counter').val(charcounter);
     
-    } else if (max_limt === 140) {
-      $('#tweet-counter').css("color", "red");
-      charcounter --;
-      $('#tweet-counter').text(charcounter);
+    // } else if (max_limt === 140) {
+    //   $('#tweet-counter').css("color", "red");
+    //   charcounter --;
+    //   $('#tweet-counter').text(charcounter);
       
-    } else {
-      charcounter ++;
-      $('#tweet-counter').text(charcounter);
+    // } else {
+    //   charcounter ++;
+    //   $('#tweet-counter').text(charcounter);
+    // }
+
+    resultCount = limit - $tweetTextLength;
+    if (resultCount > 0){
+      $('#tweet-counter').text($tweetTextLength);
+    } else if (resultCount === 0) {
+      $('#tweet-counter').text(0);
+    }  else {
+      $('#tweet-counter').css("color", "red");
+      $('#tweet-counter').text(resultCount);
     }
   })
+
+  
 });
