ea2b79abccc1d0d00074e4b1e4857754ca780717
Khushbu <khushbu.thakkar@outlook.com>
2021-09-10 04:34:24 +0000

    Code cleanup and Add comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,28 +1,11 @@
 $(document).ready(function() {
-
-  const limit = 140;
+  const max_limit = 140;
   let resultCount = 0;
-  $('#tweet-text').on('keypress', function(event) {  
-     let $charcounter = Number($('#tweet-counter').val());
-    // let max_limt = Number($('#max_limit').val());
-     let $tweetTextLength = Number($('#tweet-text').val().length) + 1;
-    // if(charcounter >= limit){
-    //   charcounter = 0;
-    //   max_limt = limit;
-    //   $('#max_limit').val(max_limt);
-    //   $('#tweet-counter').val(charcounter);
-    
-    // } else if (max_limt === 140) {
-    //   $('#tweet-counter').css("color", "red");
-    //   charcounter --;
-    //   $('#tweet-counter').text(charcounter);
-      
-    // } else {
-    //   charcounter ++;
-    //   $('#tweet-counter').text(charcounter);
-    // }
 
-    resultCount = limit - $tweetTextLength;
+  // Characters count on keypress event
+  $('#tweet-text').on('keypress', function(event) {  
+    let $tweetTextLength = $('#tweet-text').val().length + 1;
+    resultCount = max_limit - $tweetTextLength;
     if (resultCount > 0){
       $('#tweet-counter').text($tweetTextLength);
     } else if (resultCount === 0) {
@@ -31,7 +14,5 @@ $(document).ready(function() {
       $('#tweet-counter').css("color", "red");
       $('#tweet-counter').text(resultCount);
     }
-  })
-
-  
+  })  
 });
