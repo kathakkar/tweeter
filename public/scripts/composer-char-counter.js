a81b4f18bd2690c31f4073a9c3214b33c9ed62d3
Khushbu <khushbu.thakkar@outlook.com>
2021-09-10 06:58:18 +0000

    Add code to set color

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -6,11 +6,10 @@ $(document).ready(function() {
   $('#tweet-text').on('keypress', function(event) {  
     let $tweetTextLength = $('#tweet-text').val().length + 1;
     resultCount = max_limit - $tweetTextLength;
-    if (resultCount > 0){
+    if (resultCount >= 0){
+      $('#tweet-counter').css("color", "#545149");
       $('#tweet-counter').text($tweetTextLength);
-    } else if (resultCount === 0) {
-      $('#tweet-counter').text(0);
-    }  else {
+    } else {
       $('#tweet-counter').css("color", "red");
       $('#tweet-counter').text(resultCount);
     }
